97e0c7b5facc71a83528545115f78a40cf80f59d
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-02-10 12:37:58 -0800

    Hello World

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
@@ -6,5 +8,9 @@ app.set('view engine', 'ejs')
 app.use(expressLayouts)
 
 app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`)
+})
