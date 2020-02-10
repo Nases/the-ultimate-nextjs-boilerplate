687af07dc621fb556aac555357035f67e2867565
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-02-10 13:38:06 -0800

    ⭐️

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
 const app = express()
+
+// mongoose.connect('mongodb+srv://nases:qweqwedD@nases-group-llc-bophr.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err))
+
+// mongoose.connect('mongodb+srv://nases2:qweqwedD@nases-group-llc-bophr.azure.mongodb.net/test?retryWrites=true&w=majority').then(() => {
+//   console.log("Connected to Database");
+// }).catch((err) => {
+//   console.log("Not Connected to Database ERROR! ", err);
+// });
 
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
