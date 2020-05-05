583dc07b7f592893e6a049bdbd07994b97d5f1d0
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-05-05 14:19:32 -0700

    '⭐️'

--- client/components/partials/Header/Header.js
+++ client/components/partials/Header/Header.js
@@ -54,7 +54,7 @@ export default () => {
   return (
     <>
       <div className='headerBefore'></div>
-      <nav className="header bg-white shadow-sm sticky top-0">
+      <nav className="header bg-white shadow-sm sm:sticky top-0">
         <div className="max-w-7xl mx-auto pt-1 px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             {/* logo & main menu items */}
