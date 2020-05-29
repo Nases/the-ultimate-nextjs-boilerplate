84d4032bd5f618238fcf70a004e157a8a813439b
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-05-28 19:44:55 -0700

    '⭐️'

--- client/pages/api/signup.js
+++ client/pages/api/signup.js
@@ -3,7 +3,7 @@ const User = require('../../models/User')
 
 
 export default async (req, res) => {
-  res.status(200).json(req.body)
+  res.status(200).send('asds')
   // req.body.email
   // req.body.password
 
