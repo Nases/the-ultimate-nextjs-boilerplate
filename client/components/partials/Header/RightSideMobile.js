8c9e8c8ecf0f2c792a654002a0ee0b61258e82c4
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-06-12 20:38:50 -0700

    '⭐️'

--- client/components/partials/Header/RightSideMobile.js
+++ client/components/partials/Header/RightSideMobile.js
@@ -73,31 +73,33 @@ const RightSideMobile = () => {
       <div className="pt-2 pb-3">
         <MainMenuLinks isMobile={true} />
       </div>
+      <div className="pt-3 pb-3 border-t border-common">
+        {
+          userData.isAuth ?
+            // logged in
+            <>
+              <div className="flex items-center px-4">
+                <div className="flex-shrink-0">
+                  <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium leading-6 text-common-dark">Tom Cook</div>
+                  <div className="text-sm font-medium leading-5 text-common-light">tom@example.com</div>
+                </div>
+              </div>
+              <div className="mt-3" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                <ProfileMenuLinks isMobile={true} />
+              </div>
+            </>
+            :
+            // signed out
+            <div>
+              <LoginButton isMobile={true} />
+              <SignUpButton isMobile={true} />
+            </div>
+        }
+      </div>
 
-      {
-        userData.isAuth ?
-          // logged in
-          < div className="pt-4 pb-3 border-t border-common">
-            <div className="flex items-center px-4">
-              <div className="flex-shrink-0">
-                <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium leading-6 text-common-dark">Tom Cook</div>
-                <div className="text-sm font-medium leading-5 text-common-light">tom@example.com</div>
-              </div>
-            </div>
-            <div className="mt-3" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-              <ProfileMenuLinks isMobile={true} />
-            </div>
-          </div>
-          :
-          // signed out
-          <div>
-            <LoginButton isMobile={true} />
-            <SignUpButton isMobile={true} />
-          </div>
-      }
     </div>
   )
 }
