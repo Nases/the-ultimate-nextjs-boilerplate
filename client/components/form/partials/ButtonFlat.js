1596910b19560cec605ec749c096de2d3d938be5
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-06-17 22:17:32 -0700

    '⭐️'

--- client/components/form/partials/ButtonFlat.js
+++ client/components/form/partials/ButtonFlat.js
@@ -1,8 +1,8 @@
 const ButtonFlat = (props) => {
   return (
-    <a {...props} href="#" className="inline-flex items-center font-semibold uppercase hover:text-primary h-10 px-3 py-2 text-sm leading-4 text-common-dark transition ease-in-out duration-150">
+    <button {...props} href="#" className="inline-flex items-center font-semibold uppercase hover:text-primary h-10 px-3 py-2 text-sm leading-4 text-common-dark transition ease-in-out duration-150">
       {props.children}
-    </a >
+    </button >
   )
 }
 
--- client/components/partials/User/Password.js
+++ client/components/partials/User/Password.js
@@ -1,31 +1,56 @@
+import { useState } from 'react'
 import ButtonFlat from '../../form/partials/ButtonFlat'
 import Button from '../../form/partials/Button'
 
 
+
+
 const Password = () => {
+  const [altMenuActive, setAltMenuActive] = useState(false)
+
+  const openAltMenu = () => {
+    setAltMenuActive(true)
+  }
+
+  const closeAltMenu = () => {
+    setAltMenuActive(false)
+  }
+
+  const InfoMenu = () => {
+    return (
+      <div className="bg-gray-50 grid grid-cols-6 gap-4 px-6 py-8">
+        <div className='col-span-1 text-common text-sm'>
+          Password
+        </div>
+        <div className='col-span-5 text-common'>
+          Last updated May 17, 2017
+        </div>
+      </div>
+    )
+  }
+
+
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-        <div class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap">
-          <div class="ml-4 mt-2">
-            <h3 class="text-lg uppercase leading-6 font-medium text-gray-900">
+    <div className="rounded overflow-hidden shadow-md">
+      <div className="bg-white px-4 py-5 border-b border-gray-100">
+        <div className="flex items-center justify-between flex-wrap sm:flex-no-wrap">
+          <div>
+            <h3 className="text-lg uppercase font-bold text-common-dark">
               Password
             </h3>
+            <p className='text-common-light text-sm mt-1'>
+              We recommend updating your password periodically to prevent unauthorized access.
+            </p>
           </div>
-          <div class="ml-4 mt-2 flex-shrink-0">
-            <ButtonFlat>
-              <i class="fas fa-edit"></i>
-                Update
+          <div className={altMenuActive ? 'hidden' : ''}>
+            <ButtonFlat onClick={openAltMenu}>
+              <i className="fas fa-edit"></i>
+              Update
             </ButtonFlat>
           </div>
         </div>
       </div>
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
+      {altMenuActive ? '' : <InfoMenu />}
     </div>
   )
 }
