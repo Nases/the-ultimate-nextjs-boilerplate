e04bb604a8926969ec3c21e093dfefbf7abe1c9c
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-07-26 19:59:34 -0700

    '⭐️'

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -4,7 +4,7 @@ const Button = (props) => {
     primary: 'text-white bg-primary hover:bg-primary-dark active:bg-primary-darker',
     secondary: 'text-primary bg-white border border-primary hover:text-primary-dark hover:border-primary-dark active:text-primary-darker active:border-primary-darker',
     link: 'text-common-dark font-semibold hover:text-primary',
-    gamifyPrimary: 'text-white border-b-4 border-primary-dark bg-primary hover:bg-primary-dark hover:border-primary-darker active:border-b-0 active:border-t-4'
+    gamifyPrimary: 'text-white border-b-4 border-primary-dark bg-primary hover:bg-primary-dark hover:border-orange-800 active:border-b-0 active:border-t-4'
   }
 
   const defaultColor = colors['primary']
@@ -15,7 +15,7 @@ const Button = (props) => {
       className={`
         ${props.disabled && 'button-disabled opacity-50 cursor-default'}
         ${colors[props.color] || defaultColor}
-        w-full flex justify-center py-2 px-4 h-10 text-sm font-medium rounded-md transition duration-150 ease-in-out
+        w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md transition duration-150 ease-in-out
         ${props.className}
       `}
     >
