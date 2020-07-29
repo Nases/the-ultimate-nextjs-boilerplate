eaa51413e2a2948f99e4c249abbb2221e56c06b6
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-07-28 18:21:37 -0700

    '⭐️'

--- client/pages/settings.js
+++ client/pages/settings.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout'
 import companyInfo from '../assets/company-info'
 import EnsureAuth from '../components/utils/EnsureAuth'
 import Template from '../components/Card/UserOptionsCard/Template'
+import UserSidebar from '../components/partials/User/UserSidebar'
 
 const Dashboard = () => {
 
@@ -12,8 +13,14 @@ const Dashboard = () => {
     <EnsureAuth>
       <Layout title={title} description={description}>
         <div className='max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-8'>
-          {/* <ChangePassword /> */}
-          <Template />
+          <div className='grid grid-cols-4 gap-4'>
+            <div className='col-span-1'>
+              <UserSidebar />
+            </div>
+            <div className='col-span-3'>
+              <Template />
+            </div>
+          </div>
         </div>
       </Layout>
     </EnsureAuth>
