7de45b132e9a1355ed19d097bee869cbca47fe51
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-07-29 02:54:21 -0700

    '⭐️'

--- server/routes/index.js
+++ server/routes/index.js
@@ -139,44 +139,56 @@ router.post('/change-password', (req, res) => {
   }
 })
 
+
+
+
+
+
+
+const nodemailer = require("nodemailer")
+
+async function sendMail({
+  from,
+  to,
+  subject,
+  text,
+  html,
+}) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: parseInt(process.env.MAIL_PORT, 10),
+    secure: !(process.env.MAIL_SECURE === 'false'),
+    auth: {
+      user: process.env.MAIL_USER_NAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  })
+
+  let info = await transporter.sendMail({
+    from: from || process.env.MAIL_NAME,
+    to: to,
+    subject: subject,
+    text: text,
+    html: html,
+  }, (err, info) => {
+    if (err) {
+      console.log(err)
+    } else if (info) {
+      console.log(info)
+    }
+  })
+}
+
+
 router.post('/forgot_password', (req, res) => {
-  const nodemailer = require("nodemailer")
-
-  // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    // let testAccount = await nodemailer.createTestAccount()
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: parseInt(process.env.MAIL_PORT, 10),
-      secure: !(process.env.MAIL_SECURE === 'false'), // true for 465, false for other ports
-      auth: {
-        user: process.env.MAIL_USER_NAME, // generated ethereal user
-        pass: process.env.MAIL_PASSWORD, // generated ethereal password
-      },
-    })
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <nextjsexpressboilerplate@gmail.com>', // sender address
-      to: "hasan@hasansefaozalp.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    })
-
-    console.log("Message sent: %s", info.messageId)
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
-
-  main().catch(console.error)
+  sendMail({
+    to: "hasan@hasansefaozalp.com",
+    subject: "Hello ✔ test test",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>",
+  })
+    .then(console.log('wee it worked'))
+    .catch(console.error)
 
   res.send('weeeee forgot password progresssssss')
 })
