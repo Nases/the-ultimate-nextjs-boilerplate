5a3c0aa51276fd168dc3bec33cba3850fcd67133
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-08-01 22:10:31 -0700

    '⭐️'

--- client/components/utils/EnsureForgotPasswordChangePassword.js
+++ client/components/utils/EnsureForgotPasswordChangePassword.js
@@ -7,10 +7,12 @@ const EnsureForgotPasswordChangePassword = ({ children }) => {
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(true)
   const { email, forgotPasswordToken } = router.query
-
+  console.log(router.query)
+  console.log(Object.keys(router.query).length)
 
   useEffect(() => {
     if (email && forgotPasswordToken) {
+      console.log('yo yo 2')
       userUtils.ensureForgotPasswordChangePassword(email, forgotPasswordToken)
         .then(() => {
           setIsLoading(false)
@@ -19,6 +21,9 @@ const EnsureForgotPasswordChangePassword = ({ children }) => {
           // console.log(err)
           Router.push('/')
         })
+    } else if (Object.keys(router.query).length > 0) {
+      console.log('yo yo')
+      if (!email && !forgotPasswordToken) Router.push('/')
     }
   }, [email, forgotPasswordToken])
 
