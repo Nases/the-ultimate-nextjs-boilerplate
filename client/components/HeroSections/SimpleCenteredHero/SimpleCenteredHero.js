bcc4bed1dbcb4a34d9c788bab1f0056d87a905a1
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-08-03 07:09:41 -0700

    '⭐️'

--- client/components/HeroSections/SimpleCenteredHero/SimpleCenteredHero.js
+++ client/components/HeroSections/SimpleCenteredHero/SimpleCenteredHero.js
@@ -15,24 +15,12 @@ const SimpleCenteredHero = () => {
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
             </p>
-            {/* <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-              <div className="rounded-md shadow">
-                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                  Get started
-                </a>
-              </div>
-              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                  Live demo
-                </a>
-              </div>
-            </div> */}
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
-                <Button color='gamifyPrimary' size='xl'>Get started</Button>
+                <Button color='gamifyPrimary' size='xl' className='w-full'>Get started</Button>
               </div>
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <Button color='gamifySecondary' size='xl'>Live demo</Button>
+                <Button color='gamifySecondary' size='xl' className='w-full'>Live demo</Button>
               </div>
             </div>
           </div>
--- client/pages/policies/terms.js
+++ client/pages/policies/terms.js
@@ -5,18 +5,22 @@ import companyInfo from '../../assets/company-info'
 const Terms = () => {
   var title = `Terms and Conditions | ${companyInfo.name}`
   var description = 'See our terms of our service and how they relate to you.'
+  const lastUpdated = 'Aug 2, 2020'
   return (
     <Layout title={title} description={description}>
       <LayoutIndent>
-        <div className='text-common-dark bg-white py-2 px-6 my-6 mx-4 rounded-lg'>
+        <div className='prose max-w-none text-common bg-white py-6 md:py-10 md:px-8 my-6 px-6 rounded-lg'>
           <h1>Terms and Conditions</h1>
+          <h3>Last updated on {lastUpdated}</h3>
           <p>Welcome to {companyInfo.name}!</p>
           <p>These terms and conditions outline the rules and regulations for the use of {companyInfo.website}'s Website, located at {companyInfo.website}.</p>
           <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use {companyInfo.name} if you do not agree to take all of the terms and conditions stated on this page.</p>
           <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+
           <h2>Cookies</h2>
           <p>We employ the use of cookies. By accessing {companyInfo.name}, you agreed to use cookies in agreement with the {companyInfo.website}'s Privacy Policy.</p>
           <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+
           <h2>License</h2>
           <p>Unless otherwise stated, {companyInfo.website} and/or its licensors own the intellectual property rights for all material on {companyInfo.name}. All intellectual property rights are reserved. You may access this from {companyInfo.name} for your own personal use subjected to restrictions set in these terms and conditions.</p>
           <p>You must not:</p>
@@ -48,7 +52,7 @@ const Terms = () => {
           </ul>
           <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</p>
           <p>We may consider and approve other link requests from the following types of organizations:</p>
-          <ul className='list-disc list-inside'>
+          <ul>
             <li>commonly-known consumer and/or business information sources;</li>
             <li>dot.com community sites;</li>
             <li>associations or other groups representing charities;</li>
@@ -61,26 +65,32 @@ const Terms = () => {
           <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.</p>
           <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to {companyInfo.website}. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
           <p>Approved organizations may hyperlink to our Website as follows:</p>
-          <ul className='list-disc list-inside'>
+          <ul>
             <li>By use of our corporate name; or</li>
             <li>By use of the uniform resource locator being linked to; or</li>
             <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
           </ul>
           <p>No use of {companyInfo.website}'s logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+
           <h2>iFrames</h2>
           <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
+
           <h2>Content Liability</h2>
           <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+
           <h2>Your Privacy</h2>
           <p>Please read Privacy Policy</p>
+
           <h2>Reservation of Rights</h2>
           <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+
           <h2>Removal of links from our website</h2>
           <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
           <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+
           <h2>Disclaimer</h2>
           <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
-          <ul className='list-disc list-inside'>
+          <ul>
             <li>limit or exclude our or your liability for death or personal injury;</li>
             <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
             <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
