e7c92e65c7f19ab5eff01987b214b3010b3d5f00
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-21 19:21:33 -0700

    validation merged

--- assets/validation/schemas.js
+++ assets/validation/schemas.js
@@ -1,8 +1,6 @@
-// import * as yup from 'yup'
 const yup = require('yup')
 
-
-export const SignUpSchema = yup.object().shape({
+const SignUpSchema = yup.object().shape({
   email: yup.string()
     .email('Invalid email')
     .required('Required'),
@@ -15,7 +13,7 @@ export const SignUpSchema = yup.object().shape({
     .required('Required'),
 })
 
-export const LoginSchema = yup.object().shape({
+const LoginSchema = yup.object().shape({
   email: yup.string()
     .email('Invalid email')
     .required('Required'),
@@ -25,7 +23,7 @@ export const LoginSchema = yup.object().shape({
     .required('Required')
 })
 
-export const ChangePasswordSchema = yup.object().shape({
+const ChangePasswordSchema = yup.object().shape({
   currentPassword: yup.string()
     .min(6, 'Password must be at least 6 characters')
     .max(24, 'Password can be maximum 24 characters')
@@ -39,7 +37,7 @@ export const ChangePasswordSchema = yup.object().shape({
     .required('Required'),
 })
 
-export const ChangeEmailSchema = yup.object().shape({
+const ChangeEmailSchema = yup.object().shape({
   email: yup.string()
     .email('Invalid email')
     .required('Required'),
@@ -49,7 +47,7 @@ export const ChangeEmailSchema = yup.object().shape({
     .required('Required'),
 })
 
-export const ChangePersonalInformationSchema = yup.object().shape({
+const ChangePersonalInformationSchema = yup.object().shape({
   firstName: yup.string()
     .min(1, 'First Name must be at least 1 character')
     .max(80, 'First Name can be maximum 80 characters')
@@ -60,15 +58,27 @@ export const ChangePersonalInformationSchema = yup.object().shape({
     .required('Required'),
 })
 
-export const ChangePhoneNumberSchema = yup.object().shape({
-  phoneNumber: yup.string()
-    .min(1, 'Phone Number must be at least 6 character')
-    .max(80, 'First Name can be maximum 80 characters')
-    .required('Required')
-})
-
-export const ForgotPasswordSchema = yup.object().shape({
+const ForgotPasswordSchema = yup.object().shape({
   email: yup.string()
     .email('Invalid email')
     .required('Required'),
 })
+
+const ForgotPasswordChangePasswordEnsureSchema = yup.object().shape({
+  email: yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  forgotPasswordToken: yup.string()
+    .min(20)
+    .required()
+})
+
+module.exports = {
+  SignUpSchema,
+  LoginSchema,
+  ChangePasswordSchema,
+  ChangeEmailSchema,
+  ChangePersonalInformationSchema,
+  ForgotPasswordSchema,
+  ForgotPasswordChangePasswordEnsureSchema
+}
