b3e512d2945ab10481f707372ba703c875380375
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-27 00:56:22 -0700

    '⭐️'

--- components/utils/EnsureForgotPasswordChangePassword.js
+++ components/utils/EnsureForgotPasswordChangePassword.js
@@ -2,39 +2,24 @@ import { useState, useEffect } from 'react'
 import userUtils from '../../assets/userUtils'
 import { useRouter } from 'next/router'
 
+
 const EnsureForgotPasswordChangePassword = ({ children }) => {
 
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(true)
 
-
   useEffect(() => {
     var { email, forgotPasswordToken } = router.query
 
-    if (router.query && email && forgotPasswordToken) {
+    if (email && forgotPasswordToken) {
       userUtils.ensureForgotPasswordChangePassword(email, forgotPasswordToken)
         .then(() => {
           setIsLoading(false)
         })
         .catch(err => {
-          // console.log(err)
           router.push('/')
         })
     }
-
-    const ensure = (q) => {
-      console.log('dafuq')
-      console.log(q)
-      if (!q.email) {
-        console.log(q)
-        router.push('/')
-      }
-    }
-
-    setTimeout(() => {
-      ensure(router.query)
-    }, 5000)
-
   })
 
   return (
@@ -43,5 +28,6 @@ const EnsureForgotPasswordChangePassword = ({ children }) => {
     </>
   )
 }
+
 
 export default EnsureForgotPasswordChangePassword
