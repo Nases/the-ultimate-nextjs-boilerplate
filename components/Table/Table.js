4815e2711354e4a7786ce1db3dd7d6d16ff910a9
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-09-30 16:39:07 -0700

    '⭐️'

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -1,28 +1,17 @@
+import TableHead from './TableHead'
+import TableRow from './TableRow'
+
+
 const Table = () => {
   return (
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className='bg-purple-600 text-white'>
-                <tr>
-                  <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
-                    Email
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
-                    Title
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
-                    Email
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
-                    Role
-                  </th>
-                  <th className="px-6 py-3 "></th>
-                </tr>
-              </thead>
+          <div className="shadow overflow-hidden border-b border-gray-200 rounded-lg">
+            <table className="min-w-full">
+              <TableHead />
               <tbody>
+                <TableRow />
                 <tr className="bg-white border-b">
                   <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
                     Jane Cooper
---
+++ components/Table/TableHead.js
@@ -0,0 +1,23 @@
+const TableHead = () => {
+  return (
+    <thead className='bg-gray-500 text-white'>
+      <tr>
+        <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
+          Email
+                  </th>
+        <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
+          Title
+                  </th>
+        <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
+          Email
+                  </th>
+        <th className="px-6 py-3 text-left text-xs leading-4 font-medium uppercase tracking-wider">
+          Role
+                  </th>
+        <th className="px-6 py-3 "></th>
+      </tr>
+    </thead>
+  )
+}
+
+export default TableHead
