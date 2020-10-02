01fe1f1d611df3441e3c12468f7449fe3c24e422
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-01 17:31:08 -0700

    '⭐️'

--- assets/routes/api/users.js
+++ assets/routes/api/users.js
@@ -4,7 +4,7 @@ const yup = require('yup')
 
 
 router.get('/', (req, res, next) => {
-  var { limit, sort } = req.query
+  var { limit, sort, skip } = req.query
 
 
   const schema = yup.object().shape({
@@ -14,16 +14,22 @@ router.get('/', (req, res, next) => {
       .max(1000, 'Limit can be maximum 1000'),
     sort: yup
       .string()
-      .matches(/(asc|desc)/, { excludeEmptyString: true })
+      .matches(/(asc|desc)/, { excludeEmptyString: true }),
+    skip: yup
+      .number()
+      .min(0, 'Skip must be at least 0')
+      .max(99999999, 'Skip can be maximum 99999999'),
   })
 
   schema.validate({
     limit: limit,
-    sort: sort
+    sort: sort,
+    skip: skip
   })
     .then(values => {
       User.find()
         .sort({ _id: (sort === 'asc' ? -1 : 1) })
+        .skip(values.skip || 0)
         .limit(values.limit || 20)
         .then(value => { res.send(value) })
         .catch(err => res.send(err))
