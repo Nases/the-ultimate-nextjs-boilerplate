9cbb580f5941d592593b45671d68b885aeeac1f5
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-02 23:01:28 -0700

    '⭐️'

--- components/User/UserDetails.js
+++ components/User/UserDetails.js
@@ -5,14 +5,22 @@ import Link from 'next/link'
 import axios from 'axios'
 import companyInfo from '../../assets/company-info'
 import moment from 'moment'
+import Skeleton from 'react-loading-skeleton'
 
 const UserDetails = ({ id }) => {
   const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     user || axios.post(companyInfo.serverURI + `user?id=${id}`)
-      .then(value => setUser(value.data[0]))
-      .catch(err => console.log(err))
+      .then(value => {
+        setUser(value.data[0])
+        setIsLoading(false)
+      })
+      .catch(err => {
+        console.log(err)
+        setIsLoading(false)
+      })
 
   }, [user])
 
@@ -36,37 +44,38 @@ const UserDetails = ({ id }) => {
 
 
   return (
-    user ?
-      <>
-        <div className="mb-1">
-          <Button className='pt-0 pb-0' color='link'>
-            <Link href='/admin/users'>
-              <a>
-                <i aria-hidden className="fas fa-long-arrow-alt-left"></i>
-                {' '}
+    isLoading ? <Skeleton className='mb-2' height={24} count={5} /> :
+      user ?
+        <>
+          <div className="mb-1">
+            <Button className='pt-0 pb-0' color='link'>
+              <Link href='/admin/users'>
+                <a>
+                  <i aria-hidden className="fas fa-long-arrow-alt-left"></i>
+                  {' '}
               Back
             </a>
-            </Link>
-          </Button>
-        </div>
-        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-          <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              {user.email}
-              {' '}
-              <Badge color='green'>
-                Customer
-              </Badge>
-            </h3>
-            <p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
-              User ID: {user._id}
-            </p>
+              </Link>
+            </Button>
           </div>
-          <UserDetailsRow title={'First name'} value={user.firstName} />
-          <UserDetailsRow title={'Last name'} value={user.lastName} />
-          <UserDetailsRow title={'Registration date'} value={moment(user.registrationDate).format('DD MMM YYYY')} />
-        </div>
-      </> : 'Something went wrong, please try again later.'
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+            <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                {user.email}
+                {' '}
+                <Badge color='green'>
+                  Customer
+              </Badge>
+              </h3>
+              <p className="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+                User ID: {user._id}
+              </p>
+            </div>
+            <UserDetailsRow title={'First name'} value={user.firstName} />
+            <UserDetailsRow title={'Last name'} value={user.lastName} />
+            <UserDetailsRow title={'Registration date'} value={moment(user.registrationDate).format('DD MMM YYYY')} />
+          </div>
+        </> : 'Something went wrong, please try again later.'
   )
 }
 
