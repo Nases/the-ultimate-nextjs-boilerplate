ef117618770fbac39620188884c42cc9f61679c9
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-08 01:25:56 -0700

    '⭐️'

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,11 +3,13 @@ import auth from '../../assets/middleware/auth'
 import dbConnect from '../../assets/middleware/dbConnect'
 import mongoose from 'mongoose'
 
+import User from '../../assets/models/User'
 
 const handler = nextConnect()
 
 handler.use(auth)
 handler.use((req, res) => {
+  dbConnect()
   var User = mongoose.model('User')
   User.find().then(value => res.send(value))
 })
