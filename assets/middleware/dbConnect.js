a1becd1e5ca2bdbbf7142af8f8df8c5ddeeef2c2
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-08 21:00:20 -0700

    '⭐️'

--- assets/middleware/dbConnect.js
+++ assets/middleware/dbConnect.js
@@ -1,44 +1,15 @@
-// import mongoose from 'mongoose'
-
-// export default async function dbConnect(req, res, next) {
-//   await mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//     .then(() => {
-//       console.log('MongoDB connected')
-//       next()
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// }
-
-/* This is a database connection function*/
 import mongoose from 'mongoose'
 
-const connection = {} /* creating connection object*/
-
 async function dbConnect() {
-  /* check if we have connection to our database*/
-  // if (connection.isConnected) {
-  //   return
-  // }
-
-  /* connecting to our database */
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
+  await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   }).then(() => {
     console.log('MongoDB connected')
-    // connection.isConnected = db.connections[0].readyState
-    // next()
+  }).catch(err => {
+    console.log(err)
   })
-    .catch(err => {
-      console.log(err)
-    })
-
 }
 
 export default dbConnect
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,6 @@
 import nextConnect from 'next-connect'
 import auth from '../../assets/middleware/auth'
 import dbConnect from '../../assets/middleware/dbConnect'
-
 import User from '../../assets/models/User'
 
 const handler = nextConnect()
