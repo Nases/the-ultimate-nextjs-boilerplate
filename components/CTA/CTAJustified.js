da7ec8e56368c15dfb4d1b325abdac63a5ea5707
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-10 23:03:30 -0700

    '⭐️'

--- components/CTA/CTAJustified.js
+++ components/CTA/CTAJustified.js
@@ -6,16 +6,16 @@ const CTAJustified = () => {
           <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
             Ready to dive in?
           <br />
-            <span className="text-indigo-600">Start your free trial today.</span>
+            <span className="text-orange-600">Start your free trial today.</span>
           </h2>
           <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
             <div className="inline-flex rounded-md shadow">
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-orange-600 hover:bg-orange-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Get started
               </a>
             </div>
             <div className="ml-3 inline-flex rounded-md shadow">
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-orange-600 bg-white hover:text-orange-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Learn more
               </a>
             </div>
