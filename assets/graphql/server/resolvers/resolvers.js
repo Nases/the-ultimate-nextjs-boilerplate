fadef92ad4d5ce0646d3586cb8679c22ed408f6f
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-10-30 16:58:45 -0700

    '⭐️'

--- assets/graphql/server/resolvers/resolvers.js
+++ assets/graphql/server/resolvers/resolvers.js
@@ -1,26 +1,10 @@
 import queries from './queries/queries'
 import mutations from './mutations/mutations'
-import { GraphQLScalarType } from 'graphql'
-import { Kind } from 'graphql/language'
+import Date from './scalars/Date'
 
 
 export const resolvers = {
-  Date: new GraphQLScalarType({
-    name: 'Date',
-    description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value) // value from the client
-    },
-    serialize(value) {
-      return value.getTime() // value sent to the client
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return parseInt(ast.value, 10) // ast value is always in string format
-      }
-      return null
-    }
-  }),
+  Date,
   Query: {
     ...queries
   },
---
+++ assets/graphql/server/resolvers/scalars/Date.js
@@ -0,0 +1,23 @@
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
+
+
+const Date = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  parseValue(value) {
+    return new Date(value) // value from the client
+  },
+  serialize(value) {
+    return value.getTime() // value sent to the client
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return parseInt(ast.value, 10) // ast value is always in string format
+    }
+    return null
+  }
+})
+
+
+export default Date
