8b1846653161dfc269ab154b58d1a892d1d8154f
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-02 23:44:29 -0800

    '⭐️'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
+    purgeLayersByDefault: true
   },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   variants: {
