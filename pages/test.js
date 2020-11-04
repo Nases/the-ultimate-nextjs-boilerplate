c2a07e67d5d1965af1d878df4af1665f27299c64
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-03 16:30:11 -0800

    '⭐️'

--- pages/test.js
+++ pages/test.js
@@ -1,17 +1,31 @@
 import Layout from '../components/Layout/Layout'
 import LayoutIndent from '../components/Layout/LayoutIndent'
 import { companyInfo } from '../assets/config/settings'
+import FacebookLogin from 'react-facebook-login';
 
 
 const Template = () => {
   var title = `Default title | ${companyInfo.name}`
   var description = 'Default description.'
 
+  const handleResponseFacebook = response => {
+    console.log(response)
+  }
 
   return (
     <Layout title={title} description={description}>
       <LayoutIndent>
-        Hey
+
+
+        <FacebookLogin
+          appId="632779907666090"
+          // autoLoad={true}
+          fields="name,email,picture"
+          // onClick={componentClicked}
+          callback={handleResponseFacebook}
+        />
+
+
       </LayoutIndent>
     </Layout>
   )
