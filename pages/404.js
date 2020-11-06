8215f23991cd5fb14b0bfb17e060d166fbcc2093
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-05 16:50:31 -0800

    '⭐️'

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout/Layout'
 import LayoutIndent from '../components/Layout/LayoutIndent'
 import { companyInfo } from '../assets/config/settings'
-import Svg404 from '../components/svg/Svg404'
+import Svg404 from '../components/Svg/Svg404'
 import Link from 'next/link'
 
 
